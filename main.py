0dfac5e915abd312476bceecedb0a9be1d61c592
Lingze Zeng <32753003+1019388642@users.noreply.github.com>
2023-04-21 13:56:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from naoqi import ALProxy
 import random
 import numpy as np
 import time
-
+import threading
 Nao_ip = '192.168.1.55'
 Nao_port = 9559
 ARUCO_DICT = {"DICT_5X5_50": cv2.aruco.DICT_5X5_50}#, "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
@@ -25,43 +25,48 @@ def neckRegulator(motion_proxy):
 
 def rock(motion_proxy):
   #motion_proxy.angleInterpolationWithSpeed('LHand', 1.0, 0.5)
-  motion_proxy.closeHand('LHand')
+  start = time.time()
+  while (time.time() - start < 3):
+    motion_proxy.closeHand('LHand')
   #motion_proxy.setAngles(['LHand'], [0.0], 0.9)
   time.sleep(1)
 
 def paper(motion_proxy):
   #motion_proxy.setAngles(['LHand'], [1.0], 0.9)
-  motion_proxy.openHand('LHand')
+  start = time.time()
+  while (time.time() - start < 3):
+    motion_proxy.openHand('LHand')
+    motion_proxy.angleInterpolationWithSpeed('LWristYaw', -1, 1)
   time.sleep(1)
 
 def scissors(motion_proxy):
-  motion_proxy.setAngles(['LHand'], [0.6], 0.9)
+  start = time.time()
+  while (time.time() - start < 3):
+    motion_proxy.setAngles(['LHand'], [0.5], 0.9)
+    motion_proxy.angleInterpolationWithSpeed('LWristYaw', 1, 1)
   time.sleep(1)
 
 def shakingArm(motion_proxy, tts_proxy, rand_gest):
-  word = ['rock', 'paper', 'scissor']
+  word = ['One', 'Two', 'Three']
   for i in range(3):
-    motion_proxy.angleInterpolationWithSpeed(['LHand'], [0.0], 0.9)
-    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', -0.3, 0.3)
+    #motion_proxy.angleInterpolationWithSpeed(['LHand'], [0.0], 0.9)
+    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', -0.1, 0.8)
     tts_proxy.say(word[i])
-    time.sleep(0.2)
-    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', 0.3, 0.3)
-    time.sleep(0.2)
+    #time.sleep(0.1)
+    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', 0.3, 0.8)
+    #time.sleep(0.1)
   tts_proxy.say(rand_gest)
-  time.sleep(0.2)
+  #time.sleep(0.1)
   if rand_gest == 'rock':
-    print('1')
-    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', -0.3, 0.3)
+    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', -0.3, 0.5)
     rock(motion_proxy)
     time.sleep(2)
   elif rand_gest == 'paper':
-    print('2')
-    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', -0.3, 0.3)
+    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', -0.3, 0.5)
     paper(motion_proxy)
     time.sleep(2)
   else:
-    print('3')
-    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', 0.3, 0.3)
+    motion_proxy.angleInterpolationWithSpeed('LShoulderPitch', 0.3, 0.5)
     scissors(motion_proxy)
     time.sleep(2)
 
@@ -81,43 +86,65 @@ def arTagReco(videoProxy, subscriber):
     if num == None:
       num = 0
     return num
+def judge(gesture_rand, gesture_reco):
+  if gesture_rand == 'rock':
+    if gesture_reco == 1:
+      return 'Fair'
+    elif gesture_reco == 2:
+      return 'Lose'
+    elif gesture_reco == 3:
+      return 'Win'
+  elif gesture_rand == 'paper':
+    if gesture_reco == 1:
+      return 'Lose'
+    elif gesture_reco == 2:
+      return 'Win'
+    elif gesture_reco == 3:
+      return 'Fair'
+  elif gesture_rand == 'scissor':
+    if gesture_reco == 1:
+      return 'Win'
+    elif gesture_reco == 2:
+      return 'Fair'
+    elif gesture_reco == 3:
+      return 'Lose'
+
 
 def main():
   proxy = ALProxy("ALVideoDevice", Nao_ip, Nao_port)
   tts = ALProxy("ALTextToSpeech", Nao_ip, Nao_port)
-  mem = ALProxy('ALMemory', Nao_ip, Nao_port)
-  asr = ALProxy('ALSpeechRecognition', Nao_ip, Nao_port)
+
   touch = ALProxy('ALTouch', Nao_ip, Nao_port)
-  #asr.setLanguage('English')
-  #vocabulary = ['yes', 'no', 'ok']
-  #asr.pause(True)
-  #asr.setParameter('Sensitivity', 1)
-  #asr.setParameter('NbHypotheses', 2)
-  #asr.setVocabulary(vocabulary, True)
-  #asr.pause(False)
-  #asr.get
-  #asr.subscribe('Test_ASR')
-  #asr.setAudioExpression(True)
-  #print(asr.getParameter('Sensitivity'))
-  #print(asr.getAudioExpression())
-  #time.sleep(20)
+
   subscriber = proxy.subscribeCamera("demo", 0, 3, 13, 1)
   motion_proxy = ALProxy('ALMotion', Nao_ip, Nao_port)
   neckRegulator(motion_proxy)
-  scissors(motion_proxy)
-  paper(motion_proxy)
+
   while(True):
     time.sleep(0.1)
-    #num = arTagReco(proxy, subscriber)
-    #shakingArm(motion_proxy, tts, rand_gesture)
     trigger = (touch.getStatus()[0][1])
     if trigger == True:
       print('yes')
       tts.say("OK! Let's play!")
       time.sleep(0.5)
-      rand_gesture = random.choice(['rock', 'paper', 'scissor'])
+      rand_gesture = random.choice(['rock', 'paper','scissor'])
+      time
       shakingArm(motion_proxy, tts, rand_gesture)
+
       num = arTagReco(proxy, subscriber)
+      while num == None or num == 0:
+        num = arTagReco(proxy, subscriber)
+      print(num, rand_gesture)
+      result = judge(rand_gesture, num)
+      if result == 'Fair':
+        tts.say('What a pity! You almost Win!')
+      elif result == 'Win':
+        tts.say('Congrates! Human beats AI')
+      else:
+        tts.say('No way! I win ha ha ha')
+      time.sleep(2)
+      tts.say('This round is over, do  you want to play one more time?')
+      time.sleep(1)
 
 
 
